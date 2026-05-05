516db01f64dbc7ef8902a532d4d80501eb774bd2
Maciek46513 <35327081+Maciek46513@users.noreply.github.com>
2026-05-05 14:54:44 -0400

    Add files via upload

---
+++ WordMayhem.java
@@ -0,0 +1,25 @@
+class Quiz
+
+{
+
+             public static void main(String args[])
+
+             {
+
+                         int[] arr = new int[6];
+
+ 
+
+                         arr[0] = 23;
+
+                         arr[1] = 43;
+
+                         arr[2] = 2;
+
+ 
+
+                         System.out.println( arr[0] + " " + arr[2] + " " + arr[6] );
+
+             }
+
+}
